5ef4f39d1b9d10abefc761d95a414ffc99f42d25
Gabrel Andrade <leirbagdev@outlook.com>
2025-10-31 11:26:57 -0300

    att2

--- javascript/home.js
+++ javascript/home.js
@@ -4,7 +4,6 @@ const main = document.querySelector('main')
 function mover() {
     const sideBar = document.querySelector('.side-bar');
     sideBar.classList.toggle('side-bar-move');
-    main.classList.toggle('main-corrigido')
 
 }
 
